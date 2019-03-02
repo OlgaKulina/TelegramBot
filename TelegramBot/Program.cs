495d770ec8a1a25d5e44b4d9c523ac4122a48165
student <student@104-02>
2019-03-02 10:22:30 +0200

    Add sart page

--- TelegramBot/Program.cs
+++ TelegramBot/Program.cs
@@ -1,21 +1,64 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Telegram;
+using Telegram.Bot;
+using Telegram.Bot.Args;
+using Telegram.Bot.Types.ReplyMarkups;
 
 namespace TelegramBot
 {
     class Program
     {
+
+        static TelegramBotClient botClient;
+
         static void Main(string[] args)
         {
-            // The code provided will print ‘Hello World’ to the console.
-            // Press Ctrl+F5 (or go to Debug > Start Without Debugging) to run your app.
-            Console.WriteLine("Hello World!");
-            Console.ReadKey();
+            try
+            {
+                botClient = new TelegramBotClient("787820791:AAHZ_GBXzPjgKtK6LhZiuHKp58OYO9CG-oA");
+                var bot = botClient.GetMeAsync().Result;
+                Console.WriteLine(bot.Username);
 
-            // Go to http://aka.ms/dotnet-get-started-console to continue learning how to build a console app! 
+                botClient.OnMessage += getMessage;
+                //botClient.OnCallbackQuery += getCallBack;
+                //botClient.StartReceiving();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            Console.Read();
         }
+
+
+        private static void getMessage(object sender, MessageEventArgs e)
+
+        {
+
+            if (e.Message.Text != null)
+                switch (e.Message.Text.ToLower())
+            {
+                
+                case "/reply":
+                    var markup = new ReplyKeyboardMarkup();
+
+                    KeyboardButton a = new KeyboardButton();
+                     a.Text= "Hello from Game!";
+
+                   markup.OneTimeKeyboard = true;
+                    botClient.SendTextMessageAsync(e.Message.Chat.Id, e.Message.Text, replyMarkup: markup);
+                    botClient.SendPhotoAsync(e.Message.Chat.Id, e.Message.Text, replyMarkup: markup);
+                    break;
+               
+                default:
+                    break;
+            }
+           
+
+
+        }
+
+
     }
+
 }
